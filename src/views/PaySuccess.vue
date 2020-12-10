fea751bc27f51a6eb3e2f774302a163250963b47
fanbingchuan <m13671335217@163.com>
2020-12-10 18:30:18 +0800

    支付成功

--- src/views/PaySuccess.vue
+++ src/views/PaySuccess.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div class="pay"></div>
+        <div class="pay">
+            <img src="~@/assets/images/paysuccess.png" alt="">
+            <p>订单支付成功</p>
+        </div>
     </div>
 </template>
 
@@ -15,5 +18,19 @@
         width: 100%;
         height: 1.27rem;
         background: #FFFFFF;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        img{
+            width: 0.45rem;
+            height: 0.45rem;
+        }
+        p{
+            font-size: 0.18rem;
+            color: #000000;
+            line-height: 0.25rem;
+        }
     }
 </style>
