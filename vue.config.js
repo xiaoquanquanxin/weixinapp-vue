0a75d272ef5f981a9db2105f148acd6dad1f06d5
quanxin <quanxin@pujitech.com>
2020-12-23 16:05:26 +0800

    修改编译

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,5 @@
 //  接口代理
 module.exports = {
-    devServer: {
-        proxy: {
-            // 容丽姐
-            '/bpi': {
-                target: 'http://192.168.100.208:8090/',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/bpi': ''
-                }
-            },
-            // mobile
-            '/mpi': {
-                target: 'http://192.168.100.208:5080/wechat-mobile/',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/mpi': ''
-                }
-            },
-            // 少杰
-            '/opi': {
-                target: 'http://6tccfq.natappfree.cc/',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/opi': '/api'
-                }
-            }
-        }
-    },
+    outputDir: './wechat-pay',
+    publicPath: '/wechat-pay/',
 };
