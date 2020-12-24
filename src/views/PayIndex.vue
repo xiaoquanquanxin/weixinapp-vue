cb44ec3908358f06f8f8213973b777034aca8f6d
fanbingchuan <m13671335217@163.com>
2020-12-24 14:37:51 +0800

    首页 微信支付

--- src/views/PayIndex.vue
+++ src/views/PayIndex.vue
@@ -74,7 +74,7 @@
               // 使用以上方式判断前端返回,微信团队郑重提示：
               //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
               // 完成订单
-              this.completePaidOrder();
+              // this.completePaidOrder();
             }
           }
         );
