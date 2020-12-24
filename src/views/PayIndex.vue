3bb104bfb6f9e6c2adbc82819b0a19085815e204
fanbingchuan <m13671335217@163.com>
2020-12-24 14:37:35 +0800

    首页 微信支付

--- src/views/PayIndex.vue
+++ src/views/PayIndex.vue
@@ -56,26 +56,29 @@
           "SUCCESS", "retMsg":
           ""
       }
+
       let {appId, timeStamp, nonceStr, signType, paySign} = res.payParams
-      WeixinJSBridge.invoke(
-        'getBrandWCPayRequest', {
-          "appId": appId,     //公众号名称，由商户传入
-          "timeStamp": timeStamp,         //时间戳，自1970年以来的秒数
-          "nonceStr": nonceStr, //随机串
-          "package": res.payParams.package,
-          "signType": signType,         //微信签名方式：
-          "paySign": paySign //微信签名
-        },
-        function (res) {
-          console.log(res)
-          if (res.err_msg == "get_brand_wcpay_request:ok") {
-            // 使用以上方式判断前端返回,微信团队郑重提示：
-            //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-            // 完成订单
-            this.completePaidOrder();
+      if (typeof WeixinJSBridge != "undefined") {
+        WeixinJSBridge.invoke(
+          'getBrandWCPayRequest', {
+            "appId": appId,     //公众号名称，由商户传入
+            "timeStamp": timeStamp,         //时间戳，自1970年以来的秒数
+            "nonceStr": nonceStr, //随机串
+            "package": res.payParams.package,
+            "signType": signType,         //微信签名方式：
+            "paySign": paySign //微信签名
+          },
+          function (res) {
+            console.log(res)
+            if (res.err_msg == "get_brand_wcpay_request:ok") {
+              // 使用以上方式判断前端返回,微信团队郑重提示：
+              //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+              // 完成订单
+              this.completePaidOrder();
+            }
           }
-        }
-      );
+        );
+      }
     },
     methods: {
       ...mapActions('test', [
