d98b3634f60278ddf339475e3cae296eadfba421
fanbingchuan <m13671335217@163.com>
2021-01-25 11:32:47 +0800

    客诉 11点32分

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ export const ipUri = {
   '/mpi': 'https://hachiseedland-dev.hachi-tech.com',
   // '/bpi':'https://hachiappv3-dev.pujiapp.com/api',
   '/bpi': 'http://192.168.100.208:3080', //  容丽姐 端口
+  '/wpi': 'http://asm-test.seedland.cc:8084/life-web/sso/api/workorder', //  客诉端口
   // '/bpi':'https://hachiseedland-dev.hachi-tech.com/v3/api/',
   '/opi': 'http://hachi-pay.mynatapp.cc/api/'  //  少杰 端口
 }
